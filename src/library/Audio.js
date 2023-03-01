2b58bcea88d2d39271d2a163fa8af2f6f2b4e97e
Willi-js <willi.mails.js@gamil.com>
2023-03-01 20:33:02 +0200

    Audio class update

--- src/library/Audio.js
+++ src/library/Audio.js
@@ -1,12 +1,14 @@
 import { GAME_HEIGHT, GAME_WIDTH, Root } from "../imv.js";
 
 export default class IMVAudio {
-    constructor(audiofile, loop = true) {
+    constructor(audiofile, loop = true, volume = 100) {
+        this.volume = volume * 0.01;
         this.audioFile = audiofile;
         this.loop = loop;
         this.audio = new Audio(this.audioFile);
 
         this.audio.loop = this.loop;
+        this.audio.re
     }
 
     play() {
@@ -17,5 +19,32 @@ export default class IMVAudio {
         this.audio.pause();
     }
 
+    restart() {
+        this.audio.currentTime = 0;
+    }
+    
+    setVolume(volume) {
+        this.volume = volume * 0.01;
+
+        this.audio.volume = this.volume;
+    }
+
+    maxVolume() {
+        this.volume = 1;
+
+        this.audio.volume = this.volume;
+    }
+
+    halfVolume() {
+        this.volume = 0.5;
+
+        this.audio.volume = this.volume;
+    }
+
+    lowVolume() {
+        this.volume = 0.1;
+
+        this.audio.volume = this.volume;
+    }
 
 }
