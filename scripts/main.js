e1e8a72190042bab060d05034a081238ead96775
Willi-js <willi.mails.js@gamil.com>
2023-03-01 20:00:36 +0200

    delete test audio file and audio functions

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,6 @@ imv.update_screen_size();
 const newEl = new imv.Rect(50, 50, 0, 0, imv.Root.getRoot(), 'black');
 newEl.draw();
 
-const newAudio = new imv.Audio('../loop.wav');
-
-imv.Root.event_MouseDown(() => {
-    //newAudio.play();
-    //setTimeout(() => { newAudio.pause() }, 1000)
-})
-
 function game_loop() {
     requestAnimationFrame(game_loop);
 };
